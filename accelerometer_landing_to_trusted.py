a9ee6ecfd5f789019f404d7c7951fd4819747c96
alexrhys <arhys1@wgu.edu>
2026-02-25 10:56:50 -0500

    update accelerometer_landing_to_trusted.py to use S3 buckets for source data

--- accelerometer_landing_to_trusted.py
+++ accelerometer_landing_to_trusted.py
@@ -27,13 +27,13 @@ DEFAULT_DATA_QUALITY_RULESET = """
 """
 
 # Script generated for node Accelerometer Landing
-AccelerometerLanding_node1771969292631 = glueContext.create_dynamic_frame.from_catalog(database="stedi_db", table_name="accelerometer_landing", transformation_ctx="AccelerometerLanding_node1771969292631")
+AccelerometerLanding_node1772034748328 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://arhys-d609/accelerometer/landing/"], "recurse": True}, transformation_ctx="AccelerometerLanding_node1772034748328")
 
 # Script generated for node Customer Trusted
-CustomerTrusted_node1771969318202 = glueContext.create_dynamic_frame.from_catalog(database="stedi_db", table_name="customer_trusted", transformation_ctx="CustomerTrusted_node1771969318202")
+CustomerTrusted_node1772034788718 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://arhys-d609/customer/trusted/"], "recurse": True}, transformation_ctx="CustomerTrusted_node1772034788718")
 
 # Script generated for node Customer Privacy Filter (Join)
-CustomerPrivacyFilterJoin_node1771969338416 = Join.apply(frame1=AccelerometerLanding_node1771969292631, frame2=CustomerTrusted_node1771969318202, keys1=["user"], keys2=["email"], transformation_ctx="CustomerPrivacyFilterJoin_node1771969338416")
+CustomerPrivacyFilterJoin_node1771969338416 = Join.apply(frame1=AccelerometerLanding_node1772034748328, frame2=CustomerTrusted_node1772034788718, keys1=["user"], keys2=["email"], transformation_ctx="CustomerPrivacyFilterJoin_node1771969338416")
 
 # Script generated for node Drop Unwanted Fields
 SqlQuery0 = '''
